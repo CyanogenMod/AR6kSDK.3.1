45e168515cb4233e6de773b38299f547ddf7866f
Artem Makhutov <artem@makhutov.org>
2012-04-21 13:54:27 +0200

    Updated kernel obj path to match new kernel build system from CM

--- AR6kSDK.build_3.1_RC.563/host/Android.mk
+++ AR6kSDK.build_3.1_RC.563/host/Android.mk
@@ -44,7 +44,7 @@ endif
 ifndef ATH_LINUXPATH
 #check for Nvidia-base platform
 ifeq ($(TARGET_PRODUCT),$(filter $(TARGET_PRODUCT),cm_betelgeuse))
-export ATH_LINUXPATH=$(ATH_ANDROID_ROOT)/$(TARGET_OUT_INTERMEDIATES)/kernel
+export ATH_LINUXPATH=$(ATH_ANDROID_ROOT)/$(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ
 ATH_CROSS_COMPILE_TYPE:=$(ATH_ANDROID_ROOT)/prebuilt/linux-x86/toolchain/arm-eabi-4.4.3/bin/arm-eabi-
 endif
 endif
