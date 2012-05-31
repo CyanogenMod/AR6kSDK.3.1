d6b657c72453800e5700e5bc986936c3af5157c9
Artem Makhutov <artem@makhutov.org>
2012-05-31 22:56:16 +0200

    Fixed for the Android.mk for proprietary files

--- AR6kSDK.build_3.1_RC.667-proprietary/host/Android.mk
+++ AR6kSDK.build_3.1_RC.667-proprietary/host/Android.mk
@@ -26,26 +26,50 @@ ifneq ($(TARGET_SIMULATOR),true)
 LOCAL_PATH:= $(call my-dir)
 include $(CLEAR_VARS)
 
+define include-ar6k-prebuilt
+    include $$(CLEAR_VARS)
+    LOCAL_MODULE := $(4)
+    LOCAL_MODULE_STEM := $(3)
+    LOCAL_MODULE_TAGS := optional debug eng
+    LOCAL_MODULE_CLASS := ETC
+    LOCAL_MODULE_PATH := $(2)
+    LOCAL_SRC_FILES := $(1)
+    ALL_DEFAULT_INSTALLED_MODULES += $(LOCAL_MODULE)
+    include $$(BUILD_PREBUILT)
+endef
+
+define add-ar6k-prebuilt-file
+    $(eval $(include-ar6k-prebuilt))
+endef
+
 # HW2.0 firmware
-ALL_PREBUILT += $(TARGET_OUT)/wifi/ath6k/AR6003/hw2.0/athwlan.bin.z77
-$(TARGET_OUT)/wifi/ath6k/AR6003/hw2.0/athwlan.bin.z77 : $(LOCAL_PATH)/../target/AR6003/hw2.0/bin/athwlan.bin.z77 | $(ACP)
-	$(transform-prebuilt-to-target)
+ar6k_hw20_src_dir := ../target/AR6003/hw2.0/bin
+ar6k_hw20_dst_dir := $(TARGET_OUT)/wifi/ath6k/AR6003/hw2.0
+$(call add-ar6k-prebuilt-file,$(ar6k_hw20_src_dir)/athwlan.bin.z77,$(ar6k_hw20_dst_dir),athwlan.bin.z77,athwlan20)
+$(call add-ar6k-prebuilt-file,$(ar6k_hw20_src_dir)/data.patch.hw2_0.bin,$(ar6k_hw20_dst_dir),data.patch.hw2_0.bin,athpatch20)
+$(call add-ar6k-prebuilt-file,$(ar6k_hw20_src_dir)/otp.bin.z77,$(ar6k_hw20_dst_dir),otp.bin.z77,athotp20)
+$(call add-ar6k-prebuilt-file,support/fakeBoardData_AR6003_v2_0.bin,$(ar6k_hw20_dst_dir),bdata.SD31.bin,athdata20)
+$(call add-ar6k-prebuilt-file,$(ar6k_hw20_src_dir)/athtcmd_ram.bin,$(ar6k_hw20_dst_dir),athtcmd_ram.bin,athtcmd20)
 
-ALL_PREBUILT += $(TARGET_OUT)/wifi/ath6k/AR6003/hw2.0/data.patch_hw2_0.bin
-$(TARGET_OUT)/wifi/ath6k/AR6003/hw2.0/data.patch_hw2_0.bin : $(LOCAL_PATH)/../target/AR6003/hw2.0/bin/data.patch.hw2_0.bin | $(ACP)
-	$(transform-prebuilt-to-target)
+#ALL_PREBUILT += $(TARGET_OUT)/wifi/ath6k/AR6003/hw2.0/athwlan.bin.z77
+#$(TARGET_OUT)/wifi/ath6k/AR6003/hw2.0/athwlan.bin.z77 : $(LOCAL_PATH)/../target/AR6003/hw2.0/bin/athwlan.bin.z77 | $(ACP)
+#	$(transform-prebuilt-to-target)
 
-ALL_PREBUILT += $(TARGET_OUT)/wifi/ath6k/AR6003/hw2.0/otp.bin.z77
-$(TARGET_OUT)/wifi/ath6k/AR6003/hw2.0/otp.bin.z77 : $(LOCAL_PATH)/../target/AR6003/hw2.0/bin/otp.bin.z77 | $(ACP)
-	$(transform-prebuilt-to-target)
+#ALL_PREBUILT += $(TARGET_OUT)/wifi/ath6k/AR6003/hw2.0/data.patch_hw2_0.bin
+#$(TARGET_OUT)/wifi/ath6k/AR6003/hw2.0/data.patch_hw2_0.bin : $(LOCAL_PATH)/../target/AR6003/hw2.0/bin/data.patch.hw2_0.bin | $(ACP)
+#	$(transform-prebuilt-to-target)
 
-ALL_PREBUILT += $(TARGET_OUT)/wifi/ath6k/AR6003/hw2.0/bdata.SD31.bin
-$(TARGET_OUT)/wifi/ath6k/AR6003/hw2.0/bdata.SD31.bin : $(LOCAL_PATH)/support/fakeBoardData_AR6003_v2_0.bin | $(ACP)
-	$(transform-prebuilt-to-target)
+#ALL_PREBUILT += $(TARGET_OUT)/wifi/ath6k/AR6003/hw2.0/otp.bin.z77
+#$(TARGET_OUT)/wifi/ath6k/AR6003/hw2.0/otp.bin.z77 : $(LOCAL_PATH)/../target/AR6003/hw2.0/bin/otp.bin.z77 | $(ACP)
+#	$(transform-prebuilt-to-target)
 
-ALL_PREBUILT += $(TARGET_OUT)/wifi/ath6k/AR6003/hw2.0/athtcmd_ram.bin
-$(TARGET_OUT)/wifi/ath6k/AR6003/hw2.0/athtcmd_ram.bin : $(LOCAL_PATH)/../target/AR6003/hw2.0/bin/athtcmd_ram.bin | $(ACP)
-	$(transform-prebuilt-to-target)
+#ALL_PREBUILT += $(TARGET_OUT)/wifi/ath6k/AR6003/hw2.0/bdata.SD31.bin
+#$(TARGET_OUT)/wifi/ath6k/AR6003/hw2.0/bdata.SD31.bin : $(LOCAL_PATH)/support/fakeBoardData_AR6003_v2_0.bin | $(ACP)
+#	$(transform-prebuilt-to-target)
+
+#ALL_PREBUILT += $(TARGET_OUT)/wifi/ath6k/AR6003/hw2.0/athtcmd_ram.bin
+#$(TARGET_OUT)/wifi/ath6k/AR6003/hw2.0/athtcmd_ram.bin : $(LOCAL_PATH)/../target/AR6003/hw2.0/bin/athtcmd_ram.bin | $(ACP)
+#	$(transform-prebuilt-to-target)
 
 # HW1.0 firmware. Comment them out if you don't need HW1.0
 #ALL_PREBUILT += $(TARGET_OUT)/wifi/ath6k/AR6003/hw1.0/athwlan.bin.z77
@@ -68,4 +92,8 @@ $(TARGET_OUT)/wifi/ath6k/AR6003/hw2.0/athtcmd_ram.bin : $(LOCAL_PATH)/../target/
 #$(TARGET_OUT)/wifi/ath6k/AR6003/hw1.0/athtcmd_ram.bin : $(LOCAL_PATH)/../target/AR6003/hw1.0/bin/athtcmd_ram.bin | $(ACP)
 #	$(transform-prebuilt-to-target)
 
+ar6k_hw21_src_dir :=
+ar6k_hw21_dst_dir :=
+
+include $(LOCAL_PATH)/tools/Android.mk
 endif
